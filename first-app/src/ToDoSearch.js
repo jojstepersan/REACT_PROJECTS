e243e20d23679814d3f02158bca6af95af2048cf
ODIN <stiven97.perdomo@gmail.com>
2023-02-19 10:13:06 -0500

    manejo de estados

--- first-app/src/ToDoSearch.js
+++ first-app/src/ToDoSearch.js
@@ -2,15 +2,19 @@ import react from "react";
 import './ToDoSearch.css'
 
 function ToDoSearch(){
+    const [searchValue,setSearchValue] = react.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
-    return(
+    return[
         <input 
             className="ToDoSearch" 
             placeholder="Cebolla" 
             onChange={onSearchValueChange}
-            />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 export {ToDoSearch};

8239220075b6905e67489f882da38bbb20fb1ce0
Stiven MAC <stiven97.perdomo@gmail.com>
2024-05-10 14:36:41 -0500

    create nav links

---
+++ react_router_project/src/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = ()=>{
+    return (
+        <nav>
+            <lu>
+                <li>Home</li>
+                <li>Blog</li>
+                <li>Profile</li>
+            </lu>
+        </nav>
+    )
+}
+export default Menu;
